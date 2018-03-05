825c759d93951129c65a9e16a9a708a109840ed9
MilesHan <ignatius102@qq.com>
2018-03-05 10:26:58 +0800

    update

--- index.php
+++ index.php
@@ -7,14 +7,3 @@
  */
 
 echo "test";
-
-
-_在配置文件里的特别codes_
-
-    重要的数据授权使用前应该要小心的检查。
-
-    当数据未通过验证时，为防止CORS，应该直接退出脚本；
-    或者可以输出一些JSON数据，让客户端知道请求被拒绝。
-
-    例如:
-    if ('some key' !== $_SERVER['HTTP_Key']) exit('{"err": 1, "msg": "Request Denied!"}');
